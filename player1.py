188961b15a3dc5edd99e7e557a07889dd26b20f0
nay-hey <154043375+nay-hey@users.noreply.github.com>
2024-03-13 22:56:59 +0530

    Update player1.py

--- player1.py
+++ player1.py
@@ -1,27 +1,45 @@
 import pygame
+from gui import Draw, Tetromino, Board
+
+class Player:
 
 
 
 
 
+    def isGameOver(self):
+        if any(self.grid[0]):
+            return True
+        else:
+            return False
+            
+    def updateGame(self):
+        completedRows = self.deleteCompletedRows()
+        clearedRowCount = len(completedRows)
+        self.updateScores(clearedRowCount)
+        return clearedRowCount
 
+    def deleteCompletedRows(self):
+        completedRows = []
+        for i, row in enumerate(self.grid):
+            if all(row):
+                completedRows.append(i)
+        for rowIndex in completedRows:
+            del self.grid[rowIndex]
+            self.grid.insert(0,self.emptyRow)
+        return completedRows
 
-def updateGame(self):
-    completedRows = self.deleteCompletedRows()
-    clearedRowCount = len(completedRows)
-    self.updateScores(clearedRowCount)
-    return clearedRowCount
+    def updateScores(self, clearedRowCount):
+        self.linesCleared += clearedRowCount
+        self.score += (self._lineScores[clearedRowCount -1 ]) #for now no levels introduced in tetris game
 
-def deleteCompletedRows(self):
-    completedRows = []
-    for i, row in enumerate(self.grid):
-        if all(row):
-            completedRows.append(i)
-    for rowIndex in completedRows:
-        del self.grid[rowIndex]
-        self.grid.insert(0,self.emptyRow)
-    return completedRows
+while True:
+    clearedRowCount = board.updateGame()
+    if gameOverCondition:  
+        gameOver = True
 
-def updateScores(self, clearedRowCount):
-    self.linesCleared += clearedRowCount
-    self.score += (self._lineScores[clearedRowCount -1 ]) #for now no levels introduced in tetris game
+    draw.refreshScreen(board, tetromino, tetrominonext)
+
+    if gameOver:
+        draw.drawGameOver(board)
+        pygame.display.update()
